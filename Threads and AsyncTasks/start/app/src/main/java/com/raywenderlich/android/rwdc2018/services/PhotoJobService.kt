48d58fe15657fcf63808c78a2162cf567a5ae730
LuizRamos <luizramospe@hotmail.com>
2021-01-02 17:49:02 -0300

    created the package services and the class PhotoJobService that inherit from JobService

---
+++ Threads and AsyncTasks/start/app/src/main/java/com/raywenderlich/android/rwdc2018/services/PhotoJobService.kt
@@ -0,0 +1,19 @@
+package com.raywenderlich.android.rwdc2018.services
+
+import android.app.Service
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.content.Intent
+import android.os.IBinder
+
+class PhotoJobService : JobService() {
+    override fun onStopJob(p0: JobParameters?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStartJob(p0: JobParameters?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+
+}
